84e0ba3d75f94de82bdd46d517470580c6210990
AlexanderVinokurov <alexflight295@gmail.com>
2019-08-12 20:45:01 +0300

    Добавил карту

---
+++ source/js/map.js
@@ -0,0 +1,23 @@
+var myMap;
+
+ymaps.ready(init);
+
+function init () {
+    myMap = new ymaps.Map('map', {
+        center: [59.938872, 30.323037],
+        zoom: 13,
+        controls: []
+    }),
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Собственный значок метки',
+      balloonContent: 'Это красивая метка'
+    },{
+      iconLayout: "default#image",
+      iconImageHref: '../img/map-marker.svg',
+      iconImageSize: [35, 35]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+
+};
