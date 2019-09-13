fc5751afdc92ea7aa653a9e48a46bfdfe48f9a77
Alexander <vinokurov.ag@mail.ru>
2019-09-13 16:01:01 +0300

    Добавил открытие модальных окон на странице с формой

--- source/js/validation.js
+++ source/js/validation.js
@@ -1,19 +1,41 @@
-var inputs = document.querySelectorAll("input:required");
+var requiredId = ["sur-name", "name", "mail"];
 var form = document.querySelector("form");
 var succesPopUp = form.querySelector(".pop-up--success");
-var popup = form.querySelector(".pop-up");
-var popupBtn = popup.querySelector("button");
+var failurePopUp = form.querySelector(".pop-up--failure");
+var popups = form.querySelectorAll(".pop-up");
+
+for (var i = 0; i < popups.length; i++) {
+  var popupBtn = popups[i].querySelector("button");
+  addClickHandler(popupBtn, popups[i]);
+}
 
 form.addEventListener("submit", function(evt) {
   evt.preventDefault();
-  succesPopUp.classList.remove("pop-up--close");
-  succesPopUp.classList.add("pop-up--open");
-});
-
-popupBtn.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  if (popup.classList.contains("pop-up--open")) {
-    popup.classList.remove("pop-up--open");
-    popup.classList.add("pop-up--close");
+  if (isValue()) {
+    succesPopUp.classList.remove("pop-up--close");
+    succesPopUp.classList.add("pop-up--open");
+  } else {
+    failurePopUp.classList.remove("pop-up--close");
+    failurePopUp.classList.add("pop-up--open");
   }
 });
+
+function addClickHandler(popupBtn, popup) {
+  popupBtn.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    if (popup.classList.contains("pop-up--open")) {
+      popup.classList.remove("pop-up--open");
+      popup.classList.add("pop-up--close");
+    }
+  });
+}
+
+function isValue() {
+  for (var i = 0; i < requiredId.length; i++) {
+    var input = document.getElementById(requiredId[i]);
+    if (!input.value) {
+      return false;
+    }
+  }
+  return true;
+}
